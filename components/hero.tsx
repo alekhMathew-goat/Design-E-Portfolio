6d99c6d84d1b69bc93e21e08ae7c405e2f89c9dd
alekhMathew-goat <alekhmathew@gmail.com>
2026-02-28 10:09:25 +0400

    fix hero section image width

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,8 +5,8 @@ export default function Hero() {
   return (
     <div className="mx-8 mb-8 mt-4 rounded-lg overflow-hidden  ">
       <Link href="/signup">
-        <Image src={logo} alt="Gran-assist Logo"  className="object-contain" />
-</Link>
+        <Image src={logo} alt="Gran-assist Logo" className="object-contain w-full" />
+      </Link>
     </div>
   )
 }
